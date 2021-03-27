c0a300f4d057cc3895bbe534dd5b1e86831fb7b1
Mateo Epalza <mateoepalzaramirez@gmail.com>
2021-03-27 15:11:14 -0500

    passport jwt

--- routes/users.js
+++ routes/users.js
@@ -4,14 +4,83 @@ const User = mongoose.model('User');
 const passport = require('passport');
 const utils = require('../lib/utils');
 
-// TODO
-router.get('/protected', (req, res, next) => {
+// we don't use sessions because we are using jwt's
+router.get('/protected', passport.authenticate('jwt', {session: false}), (req, res, next) => {
+    res.status(200).json({
+        success: true,
+        msg: 'You are authorized'
+    })
 });
 
 // TODO
-router.post('/login', function(req, res, next){});
+router.post('/login', function(req, res, next){
+    // We search for the user in our database
+    User.findOne({ username: req.body.username })
+        .then( user => {
+            // If the user doesn't exists
+            if(!user){
+                res.status(401).json({
+                    success: false,
+                    msg: "Could not find the user"
+                })
+            }
+            
+            // we validate the password
+            const isValid = utils.validPassword(req.body.password, user.hash, user.salt);
 
-// TODO
-router.post('/register', function(req, res, next){});
+            if(isValid){
+                // generate the jwt token
+                const tokenObject = utils.issueJWT(user);
+                // response to the frontend
+                res.status(200).json({
+                    success: true,
+                    user: user,
+                    token: tokenObject.token,
+                    expiresIn: tokenObject.expires,
+
+                })
+            }else{
+                res.status(401).json({
+                    success: false,
+                    msg: "You entered the wrong password"
+                })
+            }
+        })
+        .catch( err =>{
+            next(err);
+        })
+});
+
+router.post('/register', function(req, res, next){
+    // in here we obtain the hashed password and the salt random string 
+    const saltHash = utils.genPassword(req.body.password);
+    
+    // deconstruct the object
+    const salt = saltHash.salt;
+    const hash = saltHash.hash;
+    
+    // we create a new instance of the user
+    const newUser = new User({
+        username: req.body.username,
+        hash: hash,
+        salt: salt
+    });
+
+    // We save in the database
+    newUser.save()
+        .then((user) => {
+
+            // We issue the jwt tokew
+            const jwt = utils.issueJWT(user)
+
+            res.json({
+                success: true, 
+                user: user,
+                token:jwt.token,
+                expiresIn: jwt.expires
+            })
+        })
+        .catch(err => next(err));
+});
 
 module.exports = router;
